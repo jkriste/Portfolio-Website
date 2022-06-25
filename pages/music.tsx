faab8b2e70ae765049965b58e97c2402158ef8fb
Justin <jlkris007@gmail.com>
2022-06-24 20:51:49 -0400

    Fix /music/ endpoint

--- pages/music.tsx
+++ pages/music.tsx
@@ -3,6 +3,7 @@ import {generateRSS} from '../rssUtil';
 import {Markdown} from '../components/Markdown';
 import {PostData, loadMusicPosts, loadMarkdownFile} from '../loader';
 import {PostCard} from '../components/PostCard';
+import React from 'react';
 
 const Music = (props: {
     musicMarkdown: string;
@@ -10,10 +11,36 @@ const Music = (props: {
 }) => {
     return (
         <div className="content">
+            <Head>
+                <title>Music Reviews</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
+            <Markdown source={props.musicMarkdown}/>
+
+            <div className="section">
+                <div className="post-card-container">
+                    {props.music.map((music, j) => {
+                        return <PostCard post={music} key={j}/>;
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Music;
 
+export const getStaticProps = async () => {
+    const musicMarkdown = await loadMarkdownFile('introduction.md')
+    const music = await loadMusicPosts()
+
+    await generateRSS(music)
+
+    const props = {
+        musicMarkdown,
+        music
+    }
+
+    return {props}
+}
